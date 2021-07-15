01cbe10104ec8e1f56bbd9466509106a2e43904a
Jared Donovan <jdono100@gmail.com>
2021-07-14 19:54:56 -0500

    get and post routes set up in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,37 @@
-// This will have the GET's, better to work on this after login is setup
+const router = require('express').Router();
+const { User, Event } = require('../models');
+// const auth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  // make a promise with try/catch
+  try {
+    const eventsData = await Event.findAll({
+      attributes: ['name']
+    })
+    // map sequelize data into plain json
+    const events = eventsData.map(async (event) => await event.get({ plain: true }));
+    req.session.save(() => {
+      res.render('home', {
+        events,
+        loggedIn: req.session.loggedIn
+      });
+    })
+  } 
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const createEvent = await Event.create(req.body);
+    res.status(200).json(createEvent);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,14 @@
-/* 
-
 const router = require('express').Router();
 
 // this is to require the api file
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 
 // this is to require the homeroutes file
 const homeRoutes = require('./homeRoutes');
 
 // this is directing the URL to the filepath or for the REST services (GET or POST routes)
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+console.log(homeRoutes.stack);
+// router.use('/api', apiRoutes);
 
-module.exports = router; 
-
-*/
+module.exports = router;
