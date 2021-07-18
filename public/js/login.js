07b12f20f73ad190e2feb6f066d8ec24128f1cc2
tejeshpatel <tejeshpatel@live.com>
2021-07-18 13:50:06 -0500

    login page edits

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,22 @@
+const signUpRadio = document.querySelector('#signupradio');
+const logInRadio = document.querySelector('#loginradio');
+const signUpSection = document.querySelector('.signupsection');
+const logInSection = document.querySelector('.loginsection');
+if (logInRadio.checked = true) {
+    signUpSection.style.display = "none";
+    if (logInSection.style.display = "none") {
+        logInSection.style.display = "block";
+    }
+} 
+
+if (signUpRadio.checked = true) {
+    logInSection.style.display = "none";
+    if (signUpSection.style.display = "none") {
+        signUpSection.style.display = "block";
+    }
+} 
+
+
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
@@ -20,9 +39,37 @@ const signupFormHandler = async (event) => {
         if (response.ok) {
             document.location.replace('/home');
         } else {
-            alert(response.statusText);
+            alert("Sign up unsuccessful.");
+            console.log(response.statusText)
         }
     }
 };
 
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+const username = document.querySelector('#username-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
+
+
+if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/home');
+    } else {
+        alert("Login unsuccessful.");
+        console.log(response.statusText)    
+    }
+  }
+};
+
+
+
 document.querySelector('#submitsignup').addEventListener('click', signupFormHandler);
+document.querySelector('#submitlogin').addEventListener('click', loginFormHandler);
