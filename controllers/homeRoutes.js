276aa25bc559463d9e3fae556f90b9c5a2b5308b
tejeshpatel <tejeshpatel@live.com>
2021-07-19 18:32:37 -0500

    used GET in handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Event } = require('../models');
 // const auth = require('../utils/auth');
+const emptydays = [{day: 'Monday'}, {day: 'Tuesday'}, {day: 'Wednesday'}, {day: 'Thursday'}, {day: 'Friday'}, {day: 'Saturday'}, {day: 'Sunday'}]
 
 router.get('/', async (req, res) => {
   // make a promise with try/catch
@@ -9,16 +10,15 @@ router.get('/', async (req, res) => {
       attributes: ['name', 'date', 'time', 'user_id']
     })
     // map sequelize data into plain json
-    const events = eventsData.map(async (event) => await event.get({ plain: true }));
+    const events = eventsData.map( (event) =>  event.get({ plain: true }));
+    console.log(events);
     req.session.save(() => {
-      res.render('login', {
+      res.render('weekview', {
         events,
+        emptydays,
         loggedIn: req.session.loggedIn
       });
-      // res.render('home');
     })
-    // res.status(200).json(eventsData);
-    // return;
   }
 
   catch (err) {
@@ -27,9 +27,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/home', (req, res) => {
-  res.render('home');
-})
+// router.get('/weekview', (req, res) => {
+//   res.render('weekview');
+// })
 
 router.post('/', async (req, res) => {
   try {
