9150408d27cbfb2ebab871f6f0c8f45565db2f04
Jared Donovan <jdono100@gmail.com>
2021-07-20 19:48:45 -0500

    modal building

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
         req.session.user_id = userData.id;
         req.session.username = userData.username;
         req.session.email = userData.email;
-        req.session.logged_in = false;
+        req.session.logged_in = true;
         
         res.json({ user: userData, message: 'You are now logged in!' });
       });
---
+++ public/js/events.js
@@ -0,0 +1,4 @@
+document.getElementById('addevent-date').valueAsDate = new Date();
+let currentTime = document.getElementById('addevent-time')
+currentTime.valueAsDate = new Date();
+currentTime.toTimeString();
